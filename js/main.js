633cb05b91f0ff1a5da5c752f03372eedf1ab3fb
AbiMeGab <abbybaem@gmail.com>
2022-09-10 23:22:29 -0500

    Chore | Starting...

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let btnVal = document.getElementById("btnValidar");
+let campo1 = document.getElementById("n1");
+let campo2 = document.getElementById("n2");
+let campo3 = document.getElementById("n3");
+let res = document.getElementById("alertResultado");
+btnVal.addEventListener("click", function (e) {
+    res.innerHTML = " ";
+});
+
+// 1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
+for {}
+// 2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
+// 3. Adivinar un número entre el 1 y el 100 en el menor número de pasos posibles
+// 4. Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
+// 5. Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
+// 6. Elabora un algoritmo para leer un número y determinar si es par o impar
+
